852cd4420413d75b87856a6b5ca8942aa9ebda16
haihp02 <trunghainguyenhp02@gmail.com>
2024-11-15 14:26:59 +0700

    verify requests to store_miner_info endpoint

--- app.py
+++ app.py
@@ -65,11 +65,23 @@ class RequestValidator:
     metagraph = bt.subtensor("finney").metagraph(23)
 
     def __init__(self):
-        # In-memory storage for used nonces (consider using Redis in production)
-        self.used_nonces = defaultdict(dict)
-        self.nonce_lock = Lock()  # Lock for thread-safe access
+        # In-memory storage for used nonces (consider using Redis later)
+        self.upload_used_nonces = defaultdict(dict)
+        self.upload_nonce_lock = Lock()  # Lock for thread-safe access
+        self.store_used_nonces = defaultdict(dict)
+        self.store_nonce_lock = Lock()  # Lock for thread-safe access
 
-    async def validate_request(self, request: Request):
+    def _timestamp_check(self, received_time_ns):
+        received_time_ns = int(received_time_ns)
+        current_time_ns = time.time_ns()
+        time_difference_seconds = (current_time_ns - received_time_ns) / 1e9  # Convert nanoseconds to seconds
+
+        if time_difference_seconds > self.REQUEST_EXPIRY_LIMIT_SECONDS:
+            raise HTTPException(status_code=400, detail="Request expired")
+
+        return received_time_ns, current_time_ns
+
+    async def validate_upload_request(self, request: Request):
         body = await request.json()
 
         # Ensure required fields are in the request body
@@ -80,28 +92,65 @@ class RequestValidator:
         if "validator_uid" not in body["metadata"]:
             raise HTTPException(status_code=400, detail="Missing required 'validator_uid' in metadata")
 
-        # Perform the nonce (timestamp) check
         try:
-            received_time_ns = int(body["nonce"])
-            current_time_ns = time.time_ns()
-            time_difference_seconds = (current_time_ns - received_time_ns) / 1e9  # Convert nanoseconds to seconds
-
-            if time_difference_seconds > self.REQUEST_EXPIRY_LIMIT_SECONDS:
-                raise HTTPException(status_code=400, detail="Request expired")
-
+            # Perform the nonce (timestamp) check
+            received_time_ns, current_time_ns = self._timestamp_check(received_time_ns=body["nonce"])
             validator_uid = body["metadata"]["validator_uid"]
             # Check if nonce is already used
-            with self.nonce_lock:
-                if validator_uid in self.used_nonces and received_time_ns in self.used_nonces[validator_uid]:
+            with self.upload_nonce_lock:
+                if validator_uid in self.upload_used_nonces and received_time_ns in self.upload_used_nonces[validator_uid]:
                     raise HTTPException(status_code=400, detail="Replay attack detected")
 
                 # Store nonce as used with expiration
-                self.used_nonces[validator_uid][received_time_ns] = current_time_ns
+                self.upload_used_nonces[validator_uid][received_time_ns] = current_time_ns
 
             # Clean up expired nonces to prevent memory bloat
-            with self.nonce_lock:
-                for uid, nonces in list(self.used_nonces.items()):
-                    self.used_nonces[uid] = {
+            with self.upload_nonce_lock:
+                for uid, nonces in list(self.upload_used_nonces.items()):
+                    self.upload_used_nonces[uid] = {
+                        ts: time_ns
+                        for ts, time_ns in nonces.items()
+                        if (current_time_ns - time_ns) / 1e9 <= self.REQUEST_EXPIRY_LIMIT_SECONDS
+                    }
+
+            # Proceed with signature verification
+            validator_ss58_address = self.metagraph.hotkeys[validator_uid]
+            # Remove 'nonce' and 'signature' to get the original signed data structure
+            original_data = {k: v for k, v in body.items() if k not in {"nonce", "signature"}}
+            serialized_data = json.dumps(original_data, sort_keys=True, separators=(',', ':'))
+            message = f"{serialized_data}{validator_ss58_address}{body['nonce']}"
+            keypair = bt.Keypair(ss58_address=validator_ss58_address)
+            
+            is_verified = keypair.verify(message, body["signature"])
+        except (ValueError, KeyError, IndexError):
+            is_verified = False
+
+        if not is_verified:
+            raise HTTPException(status_code=400, detail="Cannot verify validator")
+
+    async def validate_store_request(self, request: Request):
+        body = await request.json()
+
+        # Ensure required fields are in the request body
+        if  not all(field in body for field in ["uid", "nonce", "signature"]):
+            raise HTTPException(status_code=400, detail="Missing required fields in request")
+
+        try:
+            # Perform the nonce (timestamp) check
+            received_time_ns, current_time_ns = self._timestamp_check(received_time_ns=body["nonce"])
+            validator_uid = body["uid"]
+            # Check if nonce is already used
+            with self.store_nonce_lock:
+                if validator_uid in self.store_used_nonces and received_time_ns in self.store_used_nonces[validator_uid]:
+                    raise HTTPException(status_code=400, detail="Replay attack detected")
+
+                # Store nonce as used with expiration
+                self.store_used_nonces[validator_uid][received_time_ns] = current_time_ns
+
+            # Clean up expired nonces to prevent memory bloat
+            with self.store_nonce_lock:
+                for uid, nonces in list(self.store_used_nonces.items()):
+                    self.store_used_nonces[uid] = {
                         ts: time_ns
                         for ts, time_ns in nonces.items()
                         if (current_time_ns - time_ns) / 1e9 <= self.REQUEST_EXPIRY_LIMIT_SECONDS
@@ -123,6 +172,120 @@ class RequestValidator:
             raise HTTPException(status_code=400, detail="Cannot verify validator")
 
 
+### Suggestion for later expansion, need more code review
+# class RequestValidator:
+#     REQUEST_EXPIRY_LIMIT_SECONDS = 5  # Expiry limit constant
+#     metagraph = bt.subtensor("finney").metagraph(23)
+
+#     def __init__(self):
+#         # In-memory storage for used nonces (consider using Redis later)
+#         self.used_nonces = {
+#             "upload": defaultdict(dict),
+#             "store": defaultdict(dict),
+#         }
+#         self.nonce_locks = {
+#             "upload": Lock(),
+#             "store": Lock(),
+#         }
+
+#     def _timestamp_check(self, received_time_ns):
+#         received_time_ns = int(received_time_ns)
+#         current_time_ns = time.time_ns()
+#         time_difference_seconds = (current_time_ns - received_time_ns) / 1e9  # Convert nanoseconds to seconds
+
+#         if time_difference_seconds > self.REQUEST_EXPIRY_LIMIT_SECONDS:
+#             raise HTTPException(status_code=400, detail="Request expired")
+
+#         return received_time_ns, current_time_ns
+
+#     def _nonce_replay_check(self, nonce, uid, endpoint):
+#         """Check for replay attacks and clean up expired nonces."""
+#         current_time_ns = time.time_ns()
+#         lock = self.nonce_locks[endpoint]
+#         used_nonces = self.used_nonces[endpoint]
+
+#         with lock:
+#             if uid in used_nonces and nonce in used_nonces[uid]:
+#                 raise HTTPException(status_code=400, detail="Replay attack detected")
+#             # Store the nonce
+#             used_nonces[uid][nonce] = current_time_ns
+
+#             # Clean up expired nonces
+#             for validator_uid, nonces in list(used_nonces.items()):
+#                 used_nonces[validator_uid] = {
+#                     ts: ts_time_ns
+#                     for ts, ts_time_ns in nonces.items()
+#                     if (current_time_ns - ts_time_ns) / 1e9 <= self.REQUEST_EXPIRY_LIMIT_SECONDS
+#                 }
+
+#     def _verify_signature(self, body, serialized_data, validator_uid, endpoint):
+#         """Verify the signature."""
+#         validator_ss58_address = self.metagraph.hotkeys[validator_uid]
+#         message = f"{serialized_data}{validator_ss58_address}{body['nonce']}"
+#         keypair = bt.Keypair(ss58_address=validator_ss58_address)
+#         return keypair.verify(message, body["signature"])
+
+#     async def _validate_request(self, request: Request, endpoint: str, required_fields: list, uid_field: str):
+#         """Common validation logic for all endpoints."""
+#         body = await request.json()
+
+#         # Ensure required fields are in the request body, supporting nested fields
+#         for field in required_fields:
+#             try:
+#                 # Try to access the field, possibly nested
+#                 _ = self._get_nested_data(body, field)
+#             except KeyError:
+#                 raise HTTPException(status_code=400, detail=f"Missing required field '{field}' in request")
+
+#         try:
+#             # Extract UID using the potentially nested field path
+#             validator_uid = self._get_nested_data(body, uid_field)
+
+#             # Perform the nonce (timestamp) check
+#             received_time_ns, _ = self._timestamp_check(received_time_ns=body["nonce"])
+#             self._nonce_replay_check(received_time_ns, validator_uid, endpoint)
+
+#             # Remove 'nonce' and 'signature' to get the original signed data structure
+#             original_data = {k: v for k, v in body.items() if k not in {"nonce", "signature"}}
+#             serialized_data = json.dumps(original_data, sort_keys=True, separators=(',', ':'))
+
+#             # Verify the signature
+#             is_verified = self._verify_signature(body, serialized_data, validator_uid, endpoint)
+#         except (ValueError, KeyError, IndexError):
+#             is_verified = False
+
+#         if not is_verified:
+#             raise HTTPException(status_code=400, detail="Cannot verify validator")
+
+
+#     async def validate_upload_request(self, request: Request):
+#         """Validate upload request."""
+#         await self._validate_request(
+#             request=request,
+#             endpoint="upload",
+#             required_fields=["nonce", "signature", "metadata.validator_uid"],
+#             uid_field="metadata.validator_uid",
+#         )
+
+#     async def validate_store_request(self, request: Request):
+#         """Validate store request."""
+#         await self._validate_request(
+#             request=request,
+#             endpoint="store",
+#             required_fields=["nonce", "signature", "uid"],
+#             uid_field="uid",
+#         )
+
+#     def _get_nested_data(self, data, key_path):
+#         """Retrieve nested data using a dot-separated key path."""
+#         keys = key_path.split('.')
+#         for key in keys:
+#             if key in data:
+#                 data = data[key]
+#             else:
+#                 raise KeyError(f"Key '{key}' not found in provided data.")
+#         return data
+
 # Dependency instance
 request_validator = RequestValidator()
 
@@ -140,7 +303,7 @@ BUCKET_NAME = 'nicheimage-real-caption'
 print(s3.list_buckets())
 
 @app.post("/upload-base64-item")
-async def upload_image(item: Base64Item, _=Depends(request_validator.validate_request)):
+async def upload_image(item: Base64Item, _=Depends(request_validator.validate_upload_request)):
     # try:
     #     image = base64_to_image(item.image)
     #     image = image.convert('RGB')
@@ -162,7 +325,7 @@ async def upload_image(item: Base64Item, _=Depends(request_validator.validate_re
     return {"message": "Image uploaded successfully"}
 
 @app.post("/upload-go-journey-item")
-async def upload_mid_journey_item(item: MidJourneyItem, _=Depends(request_validator.validate_request)):
+async def upload_mid_journey_item(item: MidJourneyItem, _=Depends(request_validator.validate_upload_request)):
     # try:
     #     metadata = item.metadata
     #     image: Image.Image = await get_gojourney_item(item.output)
@@ -187,7 +350,7 @@ async def upload_mid_journey_item(item: MidJourneyItem, _=Depends(request_valida
     return {"message": "Item uploaded successfully"}
 
 @app.post("/upload-llm-item")
-async def upload_llm_item(item: dict, _=Depends(request_validator.validate_request)):
+async def upload_llm_item(item: dict, _=Depends(request_validator.validate_upload_request)):
     # try:
     #     text_collection.insert_one(item)
 
@@ -195,7 +358,7 @@ async def upload_llm_item(item: dict, _=Depends(request_validator.validate_reque
     #     return {"message": "Failed to upload item", "error": e}
     return {"message": "Item uploaded successfully"}
 @app.post("/store_miner_info")
-async def store_miner_info(item: dict):
+async def store_miner_info(item: dict, _=Depends(request_validator.validate_store_request)):
     uid = item["uid"]
     print(uid, item.get("version", "no-version"))
     validator_collection.update_one(
